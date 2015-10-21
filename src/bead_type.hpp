19906ed495a40d7142192a856bfe3e2873d7524b
Suguru Kato <salmon.bioinformatics@gmail.com>
2015-10-21 17:20:56 +0900

    Add make_sorted_pair in bead_type.hpp

--- src/bead_type.hpp
+++ src/bead_type.hpp
@@ -2,7 +2,11 @@
 #define __BEAD_TYPE_HPP
 
 #include <string>
+#include <map>
 
 using BeadType = std::string;
+
+std::pair<BeadType, BeadType> make_sorted_pair(
+    const BeadType& type0, const BeadType& type1);
 
 #endif /* __BEAD_TYPE_HPP */
